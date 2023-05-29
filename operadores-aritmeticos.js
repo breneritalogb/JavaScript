9148810aa72d284b27634ec2e53d1f1d3d87bd33
Ítalo Brener <breneritalogb@gmail.com>
2023-05-29 00:55:37 -0300

    operadores aritmeticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,32 @@
+
+// OPERADORES ARITMÉTICOS
+
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// Adição:                           +
+// Subtração                         -
+// Multiplicação                     *
+// Divisão                           /
+// Exponenciação                     **
+// Resto da divisão                  %
+
+console.log( 'Soma=', primeiroNumero + segundoNumero)
+console.log('Subtração =', primeiroNumero - segundoNumero)
+console.log('Multiplicação =', primeiroNumero * segundoNumero )
+console.log('Divisão =', primeiroNumero / segundoNumero)
+console.log('Exponenciação =', primeiroNumero ** segundoNumero)
+console.log('Resto da divisão =', primeiroNumero % segundoNumero)
+
+
+// Precendência de operadores
+
+
+console.log( 3 + 7 * 3)
+console.log((10 + 7 + 8) / 3)
+
+// Biblioteca Math
+
+console.log(Math.PI)
+console.log(Math.sqrt(16))

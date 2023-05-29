52539c63f234592912a117d18ad5f75adf7e4b49
Ítalo Brener <breneritalogb@gmail.com>
2023-05-28 23:32:08 -0300

    Estudos JS

---
+++ primeiro.js
@@ -0,0 +1,14 @@
+
+// Estou imprimindo duas mensagens no terminal
+
+console.log('Hello, World!')
+console.log('Bem-Vindo ao curso de JavaScript');
+
+
+/*
+sdsadasadsads
+
+sdsadas
+fgfgfgf
+
+*/
---
+++ variaveis.js
@@ -0,0 +1,34 @@
+// VARIÁVEIS 
+
+let idade = 26; // Criando a primeira variável
+
+console.log(idade)
+
+
+// Tipos de variáveis
+
+idade = 30;           // Number
+let numero = 30;      // Number
+let altura = 1.77;    // Number
+let nome = 'Walisson' // String (texto)
+let estudando = true  // Booleano (True or false)
+
+
+
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra;
+
+console.log(outra)
+
+let nula = null;
+
+console.log(nula)
+
+var variavel = 30; // Não utilize o var, utilize (let)
+
+const constante = 100;
